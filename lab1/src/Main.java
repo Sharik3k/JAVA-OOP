b7500c7f1741dcff115db04e1221d2168f6cc127
Sharik3k <Vladyslav.senkiv@gmail.com>
2026-02-24 11:30:19 +0300

    git connect

---
+++ lab1/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Brad Pitt,1963 року народження,відомий фідльм: Формула");
+        System.out.println("Johnny Depp ,1963 року народження,відомий фільм: Пірати карбського моря");
+        System.out.println("Matthew McConaughey,1969 року народження,відомий фідльм: Інтерстелер");
+    }
+}
