c592b01ab3ce632049c4bdf79eaf84041fcab8dd
Sharik3k <Vladyslav.senkiv@gmail.com>
2026-02-24 14:14:57 +0300

    lab 2 update

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -14,27 +14,19 @@ public class Main{
     public static void task1() {
         int a = 5;
         int b = 2;
-        int sum = (a/b) + (a%b);
         System.out.println(a/b);
         System.out.println(a%b);
-        System.out.println(sum);
     }
     public static void task2(){
         int a = 45;
         int b = 10;
         int sum = (a/b) + (a%b);
-        System.out.println(a/b);
-        System.out.println(a%b);
         System.out.println(sum);
 
     }
     public static void task3(){
         double num1 = 7.983;
-        double num2 = 15.411;
-        double sum = Math.round(num1)/num2*Math.round(num2);
-        System.out.println(Math.round(Math.round(num1)));
-        System.out.println(Math.round(Math.round(num2)));
-        System.out.println(Math.round(sum));
+        System.out.println(Math.round(num1));
 
     }
 
